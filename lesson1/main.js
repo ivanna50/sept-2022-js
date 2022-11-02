070262b4246c1ed403ba8e4f0c19ea88ab60a719
ivanna 50 <jana.bagin@gmail.com>
2022-11-02 10:24:16 +0200

    add lesson1

---
+++ lesson1/main.js
@@ -0,0 +1,77 @@
+console.log ('bbbbbbbbbbb');
+
+
+let num =123
+console.log(num);
+let s1 = 'hello';
+let s2 = 'okten';
+let sentence= s1 + '  ' + s2;
+console.log(sentence );
+
+let w1 = 'hi';
+let w2 = 'it';
+let w3 = 'you can the best';
+console.log (`${w1}   ${w2}   ${w3}`);
+
+let n1 =100;
+let n2 = 200;
+let rezult = n1 +n2;
+console.log(rezult);
+
+let foo ='asd';
+console.log(typeof'asd');
+let c5 =123;
+console.log(typeof c5);
+let c3 = 'own';
+console.log(c3);
+let c4 = 'com';
+let c8 ='ua';
+let c6='true';
+let c7 ='false';
+console.log(` ${c4}  ${c8}  ${c6}  ${c7}`);
+
+let n4 = 1.1;
+console.log(n4);
+let n5 = -999;
+console.log(n5);
+let n6 = 123;
+console.log (n6);
+let n7 = 3.14
+console.log(n7);
+let n8 =2.7;
+console.log(n8);
+let firstName='Bahyn';
+let midlleName ='Ivanna';
+let lastName = 'Milanivna';
+// let person=firstName +midlleName +lastName
+console.log(`${firstName}  ${midlleName}  ${lastName }`);
+let  n10 =123;
+console.log(typeof n10);
+let wor ='file';
+console.log(typeof'file');
+let numbe=100500;
+console.log('numbe'+' ');
+
+console.log (100+200+'!');
+console.log ('!'+100+200);
+console.log (+100+ '!'+200);
+let n11='123';
+console.log(+n11);
+let a=100;
+console.log(typeof a);
+let b ='100';
+console.log(typeof b);
+let c =true ;
+console.log(typeof c);
+let nam=122222;
+console.log(nam +'ghf');
+let strNum ='1234';
+console.log(+strNum );
+console.log(100+200);
+let sl='love';
+console.log(sl);
+sl='hello';
+console.log(sl);
+
+
+

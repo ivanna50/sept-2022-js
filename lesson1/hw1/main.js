ac39bd7d6dc7950053b0f8f0232002e79c216742
ivanna 50 <jana.bagin@gmail.com>
2022-11-03 20:14:01 +0200

    add hw1

---
+++ lesson1/hw1/main.js
@@ -0,0 +1,91 @@
+let s1='hello';
+console.log (s1);
+let c3 = 'own';
+console.log(c3);
+let c4 = 'com';
+let c8 ='ua';
+let c6='true';
+let c7 ='false';
+console.log(` ${c4}  ${c8}  ${c6}  ${c7}`);
+let n4 = 1.1;
+console.log(n4);
+let n5 = -999;
+console.log(n5);
+let n6 = 123;
+console.log (n6);
+const PI= 3.14;
+let n9=16;
+console.log (n9);
+
+let n8 =2.7;
+console.log(n8);
+let firstName='Bahyn';
+let midlleName ='Ivanna';
+let lastName = 'Milanivna';
+console.log(`${firstName}  ${midlleName}  ${lastName }`);
+let a =100;
+console.log(typeof a);
+let b ='100';
+console.log(typeof b);
+let c =true ;
+console.log(100+155+'');
+console.log(typeof c);
+console.log(100+155+88);
+console.log(!!0);
+console.log(!!'0');
+console.log(!!100);
+
+console.log( 100500 + '');
+console.log( true + '');
+console.log( +true );
+console.log(100 +true );
+console.log( +false );
+let g = 'no war';
+console.log(g);
+let j = '77777';
+console.log(+j);
+let f =44444;
+console.log(f +'');
+console.log(200+200+'!');
+let h = 65;
+console.log(h);
+h =78;
+console.log(h)
+let cc = 'hello';
+console.log(cc);
+let bb= 'octovber';
+console.log( cc  +  '  '  +     bb);
+console.log (10>5);
+console.log (10===5);
+console.log (100/5);
+let ck = true;
+console.log(ck);
+console.log (typeof ck);
+let users = [
+    {id: 4,name: 'vasja',age : 31, status :true},
+    {id: 1,name: 'jana',age : 31,  status :true},
+
+    {id: 3,name: 'petja',age : 31, status :true},
+    {id: 2,name: 'vova',age : 31, status :true},
+];
+console.log(users[1].name);
+console.log(users);
+let user1 = {
+    id : 3,
+    name:'vasjaj',
+    age : 33,
+    status : true ,
+}
+console.log(user1.age );
+console.log(user1.name);
+console.log(user1.id);
+// let arr = [100200,true , 'vasja',]
+// console.log(arr);
+let arr = [100300, true , 'abd', [11 ,22 ,33,]];
+console.log(arr [0]);
+console.log(arr [3]);
+console.log(arr [3] [2]);
+let user ={ id:1, name :"vasja",age: 31,
+    wife:{name :'jana', age :32,
+    }};
+console.log(user);

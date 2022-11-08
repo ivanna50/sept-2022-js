639844dfed213b2d49b587bb859cb4afbb31ef80
ivanna 50 <jana.bagin@gmail.com>
2022-11-08 17:52:54 +0200

    add pract

---
+++ lesson3/main.js
@@ -0,0 +1,47 @@
+
+// let age = +prompt( : 'how old are you?' );
+// let color = prompt(enter color that you see?);
+// if ('gggg'){
+//     console.log('good');
+// }else {
+//     console.log('bad');
+//
+// }
+// let arr = [];
+// if (arr.length ) {
+//     console.log('array not empy');
+//
+// }else{
+//     console.log(' empy');
+// }
+// switch ('red'){
+//     case'green':
+//         console.log('go');
+//         break ;
+//     case 'yellow':
+//         console.log('wait');
+//         break ;
+//     case 'red':
+//         console.log('stop');
+//         break ;
+//     default :
+//         console.log('????');
+// }
+// let xxx = prompt ( )|| 'gggggg';
+// console.log(xxx)
+let color =prompt ('enter color' );
+if (color === 'green'){
+    console.log('go');
+}else if (color === 'yellow'){
+    console.log('wait');
+}else if ('red'){
+    console.log('stop');
+}else {
+    console.log('???')
+}
+
+
+
+
+
+

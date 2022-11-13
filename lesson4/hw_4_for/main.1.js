b3e35bbd08c1ef5e0e65102b87957364fd4cde5b
ivanna 50 <jana.bagin@gmail.com>
2022-11-13 20:09:49 +0200

    add practuka_for

--- lesson4/hw_4_for/main.1.js
+++ lesson4/hw_4_for/main.1.js
@@ -83,20 +83,20 @@
 //         i++;
 // }
 
-
-let users = [
-    {name: 'vasya', age: 31, status: false},
-    {name: 'petya', age: 30, status: true},
-    {name: 'kolya', age: 29, status: true},
-    {name: 'olya', age: 28, status: false},
-    {name: 'max', age: 30, status: true},
-    {name: 'anya', age: 31, status: false},
-    {name: 'oleg', age: 28, status: false},
-    {name: 'andrey', age: 29, status: true},
-    {name: 'masha', age: 30, status: true},
-    {name: 'olya', age: 31, status: false},
-    {name: 'max', age: 31, status: true}
-];
+//
+// let users = [
+//     {name: 'vasya', age: 31, status: false},
+//     {name: 'petya', age: 30, status: true},
+//     {name: 'kolya', age: 29, status: true},
+//     {name: 'olya', age: 28, status: false},
+//     {name: 'max', age: 30, status: true},
+//     {name: 'anya', age: 31, status: false},
+//     {name: 'oleg', age: 28, status: false},
+//     {name: 'andrey', age: 29, status: true},
+//     {name: 'masha', age: 30, status: true},
+//     {name: 'olya', age: 31, status: false},
+//     {name: 'max', age: 31, status: true}
+// ];
 // for (const user of users) {
 //         if  (user.age>30){
 //                 console.log(user)
@@ -109,13 +109,13 @@ let users = [
 //                 console.log(user)
 //         }
 //
-//
-for (const user of users) {
-    if (!user.status) {
-        console.log(user)
-    }
+// //
+// for (const user of users) {
+//     if (!user.status) {
+//         console.log(user)
+//     }
 
-}
+// }
 
 
 // - Використовуючи данні з масиву, за допомоги document.write та циклу
@@ -177,7 +177,7 @@ for (const product of products) {
 <div class="product-card">
     <h3 class="product-title">${product.title} ,price- ${ product.price}</h3>
     <div>
-    <img src="" alt="" class="product-image">
+    <img src="${product.image}" alt="" class="product-image">
     </div >
  </div>
 `)
